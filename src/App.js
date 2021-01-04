a607138a08cc7dbfdcaf63800213c4f3a0c8a45b
Clara JOSSE <clara.joss@live.fr>
2021-01-04 20:55:25 +0100

    creer contact list

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
-import Contact from './components/contact.js';
 import React from 'react'; 
-
-
-let av1 = {
- name: 'Justin Carroll',
- avatar: 'https://randomuser.me/api/portraits/men/21.jpg',
- online: true,
-}
-
-let av2 = {
-  name: 'Lester Jackson',
-  avatar: 'https://randomuser.me/api/portraits/men/79.jpg',
-  online: false,
- }
-
-let av3 = {
-  name: 'Grace Stevens',
-  avatar: 'https://randomuser.me/api/portraits/women/62.jpg',
-  online: true,
- }
+import Contact from './components/contact.js';
+import ContactList from './components/ContactList.js';
  
 function App() {
   return (
     <div className="App">
-      <Contact contact={av1}/>
-      <Contact contact={av2}/>
-      <Contact contact={av3}/>
+      <ContactList/>
     </div>
   );
 }
